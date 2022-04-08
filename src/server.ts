a43066d4d679af7a694b26b30efeec1f32477b68
matthiass2 <matias_69914081@hotmail.com>
2022-04-07 23:12:22 -0300

    feat: rework server file

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
+import { router } from './routers'
 
 const app = express()
 const port = 8000
 
-app.get('/', (req,res)=>{
-    res.send("hellow world")
-})
+app.use(express.json())
+
+app.use(router)
+
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    response.status(401).json({
+        status: "Error",
+        massage: err.message,
+    });
+}); 
+
 
 app.listen(port, ()=>console.log(`http://localhost:${port}/`));
