7600201f4e7773a7070e0a5247961b73223fda24
matthiass2 <matias_69914081@hotmail.com>
2022-04-08 23:42:38 -0300

    feat: add user repository

---
+++ src/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,19 @@
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../../UseCases/CreateUser/ICreateUserRepository";
+import { PrismaClient } from '@prisma/client'
+
+class UsersRepository implements IUsersRepository{
+    
+    private dataBase = new PrismaClient
+
+    async findByEmail(email: string): Promise<User> {
+        const user = await this.dataBase.users.findUnique({where: {email: email}})
+        return user
+    }
+
+    async save(data: User): Promise<void> {
+        await this.dataBase.users.create({data})
+    }
+}
+
+export { UsersRepository }
