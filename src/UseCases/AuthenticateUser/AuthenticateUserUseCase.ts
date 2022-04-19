93e417acaf769294701c009bbe91e66782fe6a6d
matthiass2 <matias_69914081@hotmail.com>
2022-04-19 10:04:11 -0300

    feat(use case authenticate user): modified use case to return refresh token and access token to authenticated user

--- src/UseCases/AuthenticateUser/AuthenticateUserUseCase.ts
+++ src/UseCases/AuthenticateUser/AuthenticateUserUseCase.ts
@@ -1,26 +1,36 @@
-import { User } from "../../entities/User";
 import { sign } from 'jsonwebtoken'
 import { compare } from 'bcryptjs'
 import { AuthenticateUserDTO } from "./AuthenticateUserDTO";
 import { IAuthenticateUserRepository } from "./IAuthenticateUserRepository";
+import { RefreshToken } from '../../entities/RefreshToken';
+import { RefreshTokenDTO } from '../RefreshToken/RefreshTokenDTO';
+import { IRefreshTokenRepository } from '../RefreshToken/IRefreshTokenRepository';
+class AuthenticateUserUseCase {
 
-class AuthenticateUserUseCase{
-  
-  constructor(private authenticateUserRepository: IAuthenticateUserRepository){}
+  constructor(
+    private authenticateUserRepository: IAuthenticateUserRepository,
+    private refreshTokenRepository: IRefreshTokenRepository
+  ) { }
 
-  async execute(userDto: AuthenticateUserDTO): Promise<String>{
-    
-    const userAlreadyExists = await this.authenticateUserRepository.findByEmail(userDto.email)  
-    if(!userAlreadyExists)
+  async execute(userDTO: AuthenticateUserDTO): Promise<{ access_token: String, refresh_token: RefreshTokenDTO }> {
+
+    const userAlreadyExists = await this.authenticateUserRepository.findByEmail(userDTO.email)
+    if (!userAlreadyExists)
     throw new Error('Credentials invalid!')
 
-    const passwordMatch = await compare(userDto.password, userAlreadyExists.password)
+    const passwordMatch = await compare(userDTO.password, userAlreadyExists.password)
     if (!passwordMatch)
     throw new Error('Credentials invalid!')
 
-    const token = sign({}, process.env.JWT_KEY as string, { subject: userAlreadyExists.uuid, expiresIn: '30s' })
-    
-    return token
+    await this.refreshTokenRepository.deleteById(userAlreadyExists.id)
+
+    const refresh_token = new RefreshToken({ expiryIn: 120, user_id: userAlreadyExists.id })
+
+    await this.refreshTokenRepository.save(refresh_token)
+
+    const access_token = sign({}, process.env.JWT_KEY as string, { subject: userAlreadyExists.id, expiresIn: 60 })
+
+    return { access_token, refresh_token }
   }
 }
 
