2862fb3c07d0ca33981c99aa5522bc38d1449b0d
matthiass2 <matias_69914081@hotmail.com>
2022-04-22 23:59:22 -0300

    feat(Use case): settings in controller create user

--- src/UseCases/CreateUser/CreateUserController.ts
+++ src/UseCases/CreateUser/CreateUserController.ts
@@ -1,19 +1,19 @@
 import { Request, Response } from 'express'
-import { ICreateUserDTO } from './CreateUserDTO'
-import { CreateUserUseCase }  from './CreateUserUseCase'
+import { CreateUserDTO } from './CreateUserDTO'
+import { CreateUserUseCase } from './CreateUserUseCase'
 
-class CreateUserController{
+class CreateUserController {
 
-    constructor(private createUserUsecase: CreateUserUseCase){}
+    constructor(private createUserUsecase: CreateUserUseCase) { }
 
-    async handler(request: Request, response: Response){
+    async handler(request: Request, response: Response) {
 
-        const { name, email, password }: ICreateUserDTO = request.body
-        
-        await this.createUserUsecase.execute({name, email, password})
+        const { name, email, password }: CreateUserDTO = request.body
 
-        return response.json()
+        await this.createUserUsecase.execute({ name, email, password })
 
+        return response.status(201).json({})
     }
 }
+
 export { CreateUserController }
